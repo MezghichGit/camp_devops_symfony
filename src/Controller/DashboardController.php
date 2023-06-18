153ce2eb99244f920786b5782d5071f26b4f3f14
moahmed amine mezghich <git@mezghich.com>
2023-06-18 14:49:48 +0100

    mon premier commit

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_AGENT')  ")]
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
